24f0f9c8defb332882bdd90f5478576a03835074
dadwalakshay <akshayd00@outlook.com>
2021-05-03 00:03:56 +0530

    ListModelMixin added in BaseViewSet class MRO

--- api/base/viewsets.py
+++ api/base/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import mixins, viewsets
 
 
-class BaseViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class BaseViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
     """Base ViewSet class to be inherited by other ViewSet classes."""
 
     def __init__(self, *args, **kwargs):
