cf3315ec1cb89c33039a5ba6ebb69f619a9d205b
dadwalakshay <akshayd00@outlook.com>
2022-08-20 02:54:27 +0530

    .values monkey patched, PyYAML added as dev-dependency, bumped Python version

--- api/base/managers.py
+++ api/base/managers.py
@@ -1,31 +1,8 @@
 from django.db import models
 
 
-def QUERYSET_VALUES_MONKEY_PATCH(self, *args, **kwargs):
-    ################
-    # EXPERIMENTAL #
-    ################
-    # Monkey patch to return Model object with field attributes on calling
-    # `qs.values('field_name')` i.e. `qs[0].field_name`
-    is_raw = kwargs.pop("raw", False)
-
-    args += tuple(kwargs)
-
-    clone = self._values(*args, **kwargs)
-
-    # Write a custom Iterable class, to implement desired feature.
-    # Check below link to Django's source code for reference.
-    # https://github.com/django/django/blob/7582d913e7db7f32e4cdcfafc177aa77cbbf4332/django/db/models/query.py#L92
-    clone._iterable_class = None if is_raw else models.query.ValuesIterable
-
-    return clone
-
-
 class BaseManager(models.Manager):
     """Base Manager class to be used with BaseModel class."""
 
     def __init__(self, *args, **kwargs):
-        if models.QuerySet.values is not QUERYSET_VALUES_MONKEY_PATCH:
-            models.QuerySet.values = QUERYSET_VALUES_MONKEY_PATCH
-
         super().__init__(*args, **kwargs)  # noqa
---
+++ api/base/monkey_patch.py
@@ -0,0 +1,21 @@
+from django.db.models.query import QuerySet, ValuesIterable
+
+
+def patched_values(self, *fields, **expressions):
+    # Monkey patched .values method on QuerySet to toggle usage of
+    # ValuesIterable depending upon `is_dict` expressions.
+    # Reference:
+    # 	- https://github.com/django/django/blob/stable/3.2.x/django/db/models/query.py#L838
+    is_dict = expressions.get("is_dict", True)
+
+    fields += tuple(expressions)
+
+    clone = self._values(*fields, **expressions)
+
+    if is_dict:
+        clone._iterable_class = ValuesIterable
+
+    return clone
+
+
+QuerySet.values = patched_values
