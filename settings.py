86df7d874ab7b748058b364b9c1113b357159bb0
dadwalakshay <akshayd00@outlook.com>
2024-01-01 19:02:16 +0530

    misc: startup script updated to wait until PostgreSQL container is ready

--- settings.py
+++ settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(Path(__file__).resolve(), "staticfiles")
+STATIC_ROOT = os.path.join(Path(__file__).resolve().parent, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- start.sh
+++ start.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
+function wait_for_db_migration () {
+    while ! python3 manage.py sqlflush > /dev/null 2>&1; do
+        echo "Waiting for the Database to be ready..."
+
+        sleep 5
+    done
+
+    echo "Database is ready, running migrations..."
+
+    python3 manage.py migrate --noinput
+}
+
 echo "Running Database migrations..."
-python3 manage.py migrate
+wait_for_db_migration;
 
 echo "Collecting staticfiles..."
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic --noinput
+
+echo "Intializing SuperUser..."
+python3 manage.py createsuperuser --noinput
 
 echo "Configuring and gunicorn webserver..."
 NUM_CPU=`nproc`
