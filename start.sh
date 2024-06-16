29d5fdfd967d2b3119b2b47e35368b444797d01a
dadwalakshay <akshayd00@outlook.com>
2024-06-17 04:01:40 +0530

    misc: too many changes and upgrades, refer to README.md

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 function wait_for_db_migration () {
     while ! python3 manage.py sqlflush > /dev/null 2>&1; do
         echo "Waiting for the Database to be ready..."
@@ -17,12 +17,10 @@ wait_for_db_migration;
 echo "Collecting staticfiles..."
 python3 manage.py collectstatic --noinput
 
-echo "Intializing SuperUser..."
-python3 manage.py createsuperuser --noinput
-
 echo "Configuring and gunicorn webserver..."
 NUM_CPU=`nproc`
 NUM_WORKERS=${GUNICORN_WORKERS:=$(( 2 * NUM_CPU + 1))}
+NUM_THREADS=${NUM_WORKERS}
 
-echo "Spawing $NUM_WORKERS gevent workers"
-gunicorn app.wsgi:application --bind 0.0.0.0:4444 --workers $NUM_WORKERS
+echo "Spawing $NUM_WORKERS gthread workers"
+gunicorn app.wsgi:application --bind 0.0.0.0:4444 --workers $NUM_WORKERS --threads $NUM_THREADS
